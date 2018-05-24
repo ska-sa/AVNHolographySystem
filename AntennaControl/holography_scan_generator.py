98605cf379fc96d4213f84ec1e46cadb7565468c
james-smith-za[ska-dell-laptop-mintRebecca] <smi.james.th@gmail.com>
2018-05-24 22:35:47 +0200

    Various code fixes and tidying up.
    Stopping for the night, in the meantime this is a commit to get things
    a little bit more neat and tidy.
    Added lots of docstrings (incomplete), tidied up a bit according to
    PEP8.
    
    Still need to add a bit of functionality, scan pattern is now done but
    snp file write-out isn't done yet.

--- AntennaControl/holography_scan_generator.py
+++ AntennaControl/holography_scan_generator.py
@@ -51,7 +51,7 @@ def slew(start_az, start_el, stop_az, stop_el, slew_speed, plot=False, labeltext
     :return el
 
     """
-    # TODO finish the docstring.
+    # TODO finish the docstring's return info.
     # Calculate distance, so that you can calculate the time.
     slew_distance = np.sqrt(np.square(stop_az - start_az) + np.square(stop_el - start_el))
     slew_time = int(np.floor(slew_distance / slew_speed)) + 1
@@ -77,14 +77,22 @@ def dwell(dwell_az, dwell_el, dwell_time, plot=False, labeltext="dwell", plotfor
     """
     Stay in one place for a while.
 
-    :param az:
-    :param el:
+    :param dwell_az:
+    :type dwell_az:
+    :param dwell_el:
+    :type dwell_el:
     :param dwell_time:
+    :type dwell_time:
     :param plot:
+    :type plot:
     :param labeltext:
+    :type labeltext:
     :param plotformat:
+    :type plotformat:
     :return:
+    :rtype:
     """
+    # TODO: Finish docstring.
     az = np.ones(dwell_time) * dwell_az
     el = np.ones(dwell_time) * dwell_el
     t = np.ones(dwell_time)
@@ -97,64 +105,122 @@ def dwell(dwell_az, dwell_el, dwell_time, plot=False, labeltext="dwell", plotfor
 
 
 def generate_subscan(start_az, start_el, stop_az, stop_el, n_scans, scan_speed, slew_speed, settling_time, snake=True,
-                     plot=False, debug=False):
-    # TODO: pattern looks okay, now include space for settling time.
-    if debug:
-        print "Generating subscan, from:"
-        print start_az, start_el
-        print "to:"
-        print stop_az, stop_el
-        print "\n"
+                     plot=False):
+    """
+    Generate subscan.
+
+    A subscan is the number of back-and-forth scans between boresights.
+    Assumption that the scan will always start at the bottom left and finish at the top, either left or right,
+    depending on whether it "snakes" and the number of scans.
+
+    :param start_az: Azimuth at bottom left corner of the block.
+    :type start_az: float
+    :param start_el: Elevation at the bottom left corner of the block.
+    :type start_el: float
+    :param stop_az: Azimuth at the top right corner of the block.
+    :type stop_az: float
+    :param stop_el: Elevation at the top right corner of the block.
+    :type stop_el: float
+    :param n_scans: Number of scans into which to divide the block.
+    :type n_scans: int
+    :param scan_speed: Rate at which to scan in azimuth, degrees per second.
+    :type scan_speed: float
+    :param slew_speed: Rate at which to slew in az / el (resultant), degrees per second.
+    :type slew_speed: float
+    :param settling_time: Amount of time to allocate for the dynamics of the telescope to settle, seconds.
+    :type settling_time: int
+    :param snake: Whether or not to go back and forth, i.e. not always the same direction to scan. Default True.
+    :type snake: bool
+    :param plot: Whether or not to plot the resulting pattern, default False.
+    :type plot: bool
+    :return: Tuple containing azimuth points, elevation points, time for each point, and labels for each point.
+    :rtype: np.ndarray(float), np.ndarray(float), np.ndarray(int), list
+    """
 
     az = np.array([])
     el = np.array([])
     t = np.array([])
     labels = []  # Can normal Python lists be concatenated too?
 
+    # Corner case.
     if n_scans == 1:
-        slew(start_az, start_el, stop_az, start_el, scan_speed,
-             plot=plot, labeltext="scan", plotformat="g.")
-        # if snake:
-        #     slew(stop_az, start_el, stop_az, stop_el, scan_speed,
-        #                       plot=plot, labeltext="scan", plotformat="g.")
-        # else:
-        #     slew(stop_az, start_el, start_az, stop_el, scan_speed,
-        #          plot=plot, labeltext="scan", plotformat="g.")
+        cache = slew(start_az, start_el,
+                     stop_az, start_el,
+                     scan_speed, plot=plot, labeltext="scan", plotformat="g.")
+        az = np.concatenate((az, cache[0]))
+        el = np.concatenate((el, cache[1]))
+        t = np.concatenate((t, cache[2]))
+        labels = labels + cache[3]
+
+    # TODO: Continue to handle these return values.
+
     else:
         scan_spacing = (stop_el - start_el) / (n_scans - 1)
 
         for i in range(n_scans):
             if i % 2 == 0:
-                slew(start_az, start_el + i*(scan_spacing), stop_az, start_el + i*(scan_spacing), scan_speed,
-                              plot=plot, labeltext="scan", plotformat="g.")
+                slew(start_az, start_el + i*scan_spacing,
+                     stop_az, start_el + i*scan_spacing,
+                     scan_speed, plot=plot, labeltext="scan", plotformat="g.")
             else:
                 if snake:
-                    slew(stop_az, start_el + (i-1)*(scan_spacing), stop_az, start_el + i*(scan_spacing), slew_speed,
-                         plot=plot)  # This is a slew so default label stuff is fine.
-                    dwell(stop_az, start_el + i*(scan_spacing), settling_time, plot=plot, labeltext="settling")
-                    slew(stop_az, start_el + i*(scan_spacing), start_az, start_el + i*(scan_spacing), scan_speed,
-                              plot=plot, labeltext="scan", plotformat="g.")
+                    slew(stop_az, start_el + (i-1)*scan_spacing,
+                         stop_az, start_el + i*scan_spacing,
+                         slew_speed, plot=plot)  # This is a slew so default label stuff is fine.
+                    dwell(stop_az, start_el + i*scan_spacing,
+                          settling_time, plot=plot, labeltext="settling")
+                    slew(stop_az, start_el + i*scan_spacing,
+                         start_az, start_el + i*scan_spacing,
+                         scan_speed, plot=plot, labeltext="scan", plotformat="g.")
                     if i != (n_scans - 1):
-                        slew(start_az, start_el + i * (scan_spacing), start_az, start_el + (i + 1) * (scan_spacing), slew_speed,
-                         plot=plot)  # This is a slew so default label stuff is fine.
+                        slew(start_az, start_el + i*scan_spacing,
+                             start_az, start_el + (i + 1)*scan_spacing,
+                             slew_speed, plot=plot)  # This is a slew so default label stuff is fine.
+                        dwell(start_az, start_el + (i + 1) * scan_spacing,
+                              settling_time, plot=plot, labeltext="settling")
                 else:
-                    slew(stop_az, start_el + (i-1)*(scan_spacing), start_az, start_el + i*(scan_spacing), slew_speed,
-                         plot=plot)  # This is a slew so default label stuff is fine.
-                    slew(start_az, start_el + i*(scan_spacing), stop_az, start_el + i*(scan_spacing), scan_speed,
-                              plot=plot, labeltext="scan", plotformat="g.")
+                    slew(stop_az, start_el + (i-1)*scan_spacing,
+                         start_az, start_el + i*scan_spacing,
+                         slew_speed, plot=plot)  # This is a slew so default label stuff is fine.
+                    dwell(start_az, start_el + i*scan_spacing,
+                          settling_time, plot=plot, labeltext="settling")
+                    slew(start_az, start_el + i*scan_spacing,
+                         stop_az, start_el + i*scan_spacing,
+                         scan_speed, plot=plot, labeltext="scan", plotformat="g.")
                     if i != (n_scans - 1):
-                        slew(stop_az, start_el + i * (scan_spacing), start_az, start_el + (i + 1) * (scan_spacing), slew_speed,
-                         plot=plot)  # This is a slew so default label stuff is fine.
+                        slew(stop_az, start_el + i*scan_spacing,
+                             start_az, start_el + (i + 1)*scan_spacing,
+                             slew_speed, plot=plot)  # This is a slew so default label stuff is fine.
+                        dwell(start_az, start_el + (i + 1)*scan_spacing,
+                              settling_time, plot=plot, labeltext="settling")
 
     return az, el, t, labels
 
-def generate_boresight(start_az, start_el, stop_az, stop_el, slew_speed, settling_time, dwell_time, plot=False, debug=False):
-    if debug:
-        print "Generating boresight, from:"
-        print start_az, start_el
-        print "to:"
-        print stop_az, stop_el
-        print "\n"
+
+def generate_boresight(start_az, start_el, stop_az, stop_el, slew_speed, settling_time, dwell_time, plot=False):
+    """
+
+    :param start_az:
+    :type start_az:
+    :param start_el:
+    :type start_el:
+    :param stop_az:
+    :type stop_az:
+    :param stop_el:
+    :type stop_el:
+    :param slew_speed:
+    :type slew_speed:
+    :param settling_time:
+    :type settling_time:
+    :param dwell_time:
+    :type dwell_time:
+    :param plot:
+    :type plot:
+    :return:
+    :rtype:
+    """
+    # TODO: finish docstring
+
     az = np.array([])
     el = np.array([])
     t = np.array([])
@@ -184,9 +250,13 @@ def generate_holography_scan(az_extent, el_extent, num_scans, scans_per_boresigh
     if scans_per_boresight % 2 == 0:
         sign = -1
 
+    # TODO: Insert a cross scan here.
+
     # Corner case:
     if scans_per_boresight == 1:
         raise NotImplementedError("Haven't gotten to this corner case yet.")
+      # TODO: implement this. Shouldn't be too hard...
+
 
     i = 0
     for i in range(num_subscans):
@@ -205,12 +275,20 @@ def generate_holography_scan(az_extent, el_extent, num_scans, scans_per_boresigh
                          snake=snake, plot=plot)
 
 
+    # TODO: Insert another cross scan here.
+
     return az, el, t, labels
+
 
 if __name__ == "__main__":
 
-    generate_holography_scan(raster_size_az, raster_size_el, number_of_scans, scans_per_boresight,
-                             scan_speed, slew_speed, settling_time, boresight_time, snake, plot)
+    d_az, d_el, d_time, labels = generate_holography_scan(raster_size_az, raster_size_el,
+                                                          number_of_scans, scans_per_boresight,
+                                                          scan_speed, slew_speed, settling_time,
+                                                          boresight_time, snake, plot)
 
-    plt.grid()
-    plt.show()
+    # TODO: Write out the file, as previously. Shouldn't change much.
+
+    if plot:
+        plt.grid()
+        plt.show()
